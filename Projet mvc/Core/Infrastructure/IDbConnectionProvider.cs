0531ab400f2c2cbf8ba532e790767dba07ced6c4
maxime-dcps <maximedcps@hotmail.com>
2025-04-27 02:13:12 +0200

    Implement PGSQL database connection provider

---
+++ Projet mvc/Core/Infrastructure/IDbConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Projet_mvc.Core.Infrastructure
+{
+    public interface IDbConnectionProvider
+    {
+        public Task<IDbConnection> CreateConnection();
+    }
+}
---
+++ Projet mvc/Core/Infrastructure/PGSqlDbConnectionProvider.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Projet_mvc.Core.Infrastructure
+{
+    internal class PGSqlDbConnectionProvider
+    {
+        private readonly string _connectionString;
+
+        public PGSqlDbConnectionProvider(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Default");
+        }
+        public async Task<IDbConnection> CreateConnection()
+        {
+            //var connection = new NpgsqlConnection("Host=localhost;Database=project_app_info;Username=anonyme;Password=anonyme");
+
+            var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            return connection;
+        }
+    }
+}
